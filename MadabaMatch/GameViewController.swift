5ee6a57f5c86f9924c65225c91b3ef388518dd24
jamesyoungGHusername <92231117+jamesyoungGHusername@users.noreply.github.com>
2021-12-21 15:04:17 -0700

    removed debug display of nodes and fps

--- MadabaMatch/GameViewController.swift
+++ MadabaMatch/GameViewController.swift
@@ -13,7 +13,7 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+       
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             /*if let scene = SKScene(fileNamed: "GameScene") {
@@ -34,8 +34,8 @@ class GameViewController: UIViewController {
             
             view.ignoresSiblingOrder = true
             
-            view.showsFPS = true
-            view.showsNodeCount = true
+            //view.showsFPS = true
+            //view.showsNodeCount = true
         }
     }
 
